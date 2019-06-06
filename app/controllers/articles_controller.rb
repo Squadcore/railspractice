c6d660a56fc64cef22992307acd1f2d5a28adc08
Rails Udemy Course <cracer@inbox.lv>
2019-06-06 15:43:34 +0200

    Styling done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ end
 def create
   @article = Article.new(article_params)
   if @article.save
-    flash[:notice] = "Article was sucessfully created"
+    flash[:success] = "Article was sucessfully created"
     redirect_to article_path(@article)
   else
     render 'new'
@@ -26,7 +26,7 @@ end
 def update
 
    if @article.update(article_params)
-     flash[:notice] = "Article was successfully updated"
+     flash[:success] = "Article was successfully updated"
      redirect_to article_path(@article)
    else
      render 'edit'
@@ -40,7 +40,7 @@ end
 def destroy
 
   if @article.destroy
-    flash[:notice] = "Article was successfully destroyed, boom"
+    flash[:success] = "Article was successfully destroyed, boom"
     redirect_to articles_path
   end
 end
